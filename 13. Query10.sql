db9b5f448a22256c74f468797bcae47ecb01d0fc
pranatimn <92774905+pranatimn@users.noreply.github.com>
2021-10-31 23:42:17 +0530

    Update 13. Query10.sql

--- 13. Query10.sql
+++ 13. Query10.sql
@@ -4,4 +4,16 @@ Find the Ratio of male and female athletes participated in all olympic games.
 
 */
 
- 
+ SELECT 
+    (CAST((SELECT COUNT(*) FROM olympics_history WHERE sex='M') AS FLOAT) / 
+     CAST((SELECT COUNT(*) FROM olympics_history WHERE sex='F') AS FLOAT)) 
+    AS ratioMaleFemale
+
+/*
+
+output
+
+*/
+
+"ratiomalefemale"
+2.63806661120206
