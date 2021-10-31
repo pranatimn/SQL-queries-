643ec4f1bfef8c8c8a21cbbd84efc4554372e929
pranatimn <92774905+pranatimn@users.noreply.github.com>
2021-10-31 22:58:44 +0530

    Update 11. Query8.sql

--- 11. Query8.sql
+++ 11. Query8.sql
@@ -3,3 +3,19 @@
 Which Sports were just played only once in the olympics.
 
 */
+
+select sport
+from olympics_history
+group by sport
+having count(sport)=1
+
+
+/*
+
+output
+
+*/
+
+"sport"
+"Aeronautics"
+
