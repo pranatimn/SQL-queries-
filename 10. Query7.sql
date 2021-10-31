2fdfcfd0214b8d703270227f7ba8e4694ae06504
pranatimn <92774905+pranatimn@users.noreply.github.com>
2021-10-31 19:36:19 +0530

    Update 10. Query7.sql

--- 10. Query7.sql
+++ 10. Query7.sql
@@ -5,13 +5,68 @@
 */
 
 
-select distinct sport,games,season
+select distinct sport,season
+from olympics_history where season = 'Summer'
 
 
-*/
+
+/*
 
 output
 
 */
 
+"sport","season"
+"Hockey","Summer"
+"Motorboating","Summer"
+"Lacrosse","Summer"
+"Trampolining","Summer"
+"Jeu De Paume","Summer"
+"Diving","Summer"
+"Canoeing","Summer"
+"Swimming","Summer"
+"Softball","Summer"
+"Basque Pelota","Summer"
+"Sailing","Summer"
+"Athletics","Summer"
+"Rhythmic Gymnastics","Summer"
+"Fencing","Summer"
+"Triathlon","Summer"
+"Weightlifting","Summer"
+"Figure Skating","Summer"
+"Football","Summer"
+"Handball","Summer"
+"Roque","Summer"
+"Tug-Of-War","Summer"
+"Golf","Summer"
+"Boxing","Summer"
+"Baseball","Summer"
+"Volleyball","Summer"
+"Archery","Summer"
+"Ice Hockey","Summer"
+"Art Competitions","Summer"
+"Modern Pentathlon","Summer"
+"Alpinism","Summer"
+"Basketball","Summer"
+"Badminton","Summer"
+"Aeronautics","Summer"
+"Rowing","Summer"
+"Synchronized Swimming","Summer"
+"Cycling","Summer"
+"Water Polo","Summer"
+"Croquet","Summer"
+"Equestrianism","Summer"
+"Beach Volleyball","Summer"
+"Tennis","Summer"
+"Wrestling","Summer"
+"Racquets","Summer"
+"Judo","Summer"
+"Cricket","Summer"
+"Shooting","Summer"
+"Rugby","Summer"
+"Rugby Sevens","Summer"
+"Taekwondo","Summer"
+"Gymnastics","Summer"
+"Table Tennis","Summer"
+"Polo","Summer"
 
